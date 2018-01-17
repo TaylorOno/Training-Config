6cb0ad1ac0b3593f8573fb4ec8a373dccb07acd4
Taylor <taylor.ono@cgi.com>
2018-01-17 13:49:54 -0700

    Initial commit

---
+++ src/main/java/io/scale/training/TrainingConfig/TrainingConfigApplication.java
@@ -0,0 +1,12 @@
+package io.scale.training.TrainingConfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingConfigApplication.class, args);
+	}
+}
---
+++ src/test/java/io/scale/training/TrainingConfig/TrainingConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package io.scale.training.TrainingConfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TrainingConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
